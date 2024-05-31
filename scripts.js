23e075b1f6d8717263a444c23d40f0b34a692c79
Steven McIntyre <steven.mc2225@gmail.com>
2024-05-31 07:27:39 -0700

    trying to add book to page but not working

--- scripts.js
+++ scripts.js
@@ -1,21 +1,35 @@
 // Odin Project Library
 const library = [];
+const bookshelf = document.getElementById("bookshelf");
 
 function Book(title, author, read) { // book constructor
   this.title = title;
   this.author = author;
   this.read = read;
+ 
+  const book = document.createElement("div");
+ 
+  book.classList.add("book");
+ 
+  const titleText = document.createTextNode(this.title);
+  const authorText = document.createTextNode(this.author);
+  const readText = document.createTextNode(this.read);
+
+  book.appendChild(titleText);
+  book.appendChild(authorText);
+  book.appendChild(readText);
 }
 
-// after getting user input use this to add the new book to the library
-function addBookToLibrary(book) {library.push(book)};
+function addBookToLibrary(book) {library.push(book)};// add book to library
+function tempAddToPage(book) {bookshelf.appendChild(book)};// add book to library
 
-function getBookInfo() {
+function getBookInfo() {//make new book and add to library uses prompts for user input
   let title = prompt("What is the Title of the book?");
   let author = prompt("What is the Author of the book?");
   let read = prompt("Have you read the book?");
   const book = new Book(title, author, read);
   addBookToLibrary(book);
+  tempAddToPage(book);
 }
 
 
@@ -23,7 +37,9 @@ function getBookInfo() {
 // Testing
 //
 
+getBookInfo();
 console.log(library);
+
 
 // To do
 
