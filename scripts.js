f0655055f1047dc597e2bc812da2caad54be1596
Steven McIntyre <steven.mc2225@gmail.com>
2024-06-01 19:17:43 -0700

    can get one library book into the array. Won't do a second and won't post to the page though

--- scripts.js
+++ scripts.js
@@ -1,6 +1,33 @@
 // Odin Project Library
+
+/*
+Initializing constants
+*/
 const library = [];
 const bookshelf = document.getElementById("bookshelf");
+
+// Add new book object to library array
+const addBookButton = document.querySelector("#button_addBook");
+addBookButton.addEventListener('click', (event) => {
+  console.log(event);
+  getBookInfo();
+});
+
+// Add all the books from the library array to the page.
+const fillLibraryButton = document.querySelector("#button_fillLibrary");
+fillLibraryButton.addEventListener('click', (event) => {
+  console.log(event);
+  addBooksToShelf(library);
+  console.log(library[0].title);
+});
+
+const title = document.getElementById('title').value;
+const author = document.getElementById('author').value;
+const read = document.getElementsByName(`read`).value;
+
+/*
+Constructors
+*/
 
 function Book(title, author, read) { // book constructor
   this.title = title;
@@ -20,44 +47,62 @@ function Book(title, author, read) { // book constructor
   book.appendChild(readText);
 }
 
-function addBookToLibrary(book) { // add book to library
-  library.push(book)
+/*
+Functions
+*/
+
+function addBookToLibrary(bookObject) { // add book to library
+  library.push(bookObject);
+  for (const book in library) {
+    if (library.hasOwnProperty(book)) {
+      const theBook= library[book];
+      console.log(theBook.title);
+    }
+  }
 };
 
-function tempAddToPage(book) { // add book to library
-  bookshelf.appendChild(book)
+function addBooksToShelf(library) { // add book to library
+  for (const book in library) {
+    if (library.hasOwnProperty(book)) {
+      const bookObj = library[book];
+
+      const newBook = document.createElement("div");
+    
+      newBook.classList.add("book");
+    
+      const titleText = document.createTextNode(bookObj.title);
+      const authorText = document.createTextNode(bookObj.author);
+      const readText = document.createTextNode(bookObj.read);
+
+      newBook.appendChild(titleText);
+      newBook.appendChild(authorText);
+      newBook.appendChild(readText);
+      bookshelf.appendChild(newBook);
+      
+    }
+  }
+  // bookshelf.appendChild(bookObject.book);
+  // console.log(bookObject.title);
+  // console.log(bookObject.author);
+  // console.log(bookObject.read);
+  // console.log(bookObject);
 };
 
 function getBookInfo() { //make new book and add to library uses prompts for user input
-  let title = prompt("What is the Title of the book?");
-  let author = prompt("What is the Author of the book?");
-  let read = prompt("Have you read the book?");
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const read = document.querySelector('input[name="read"]:checked').value;
+  // let newTitle = title;
+  // let newAuthor = author;
+  // let newRead = read;
   const book = new Book(title, author, read);
   addBookToLibrary(book);
-  tempAddToPage(book);
 }
 
 
-//
-// Testing
-//
+/*
+Testing
+*/
+// console.log(library);
 
-// getBookInfo();
-console.log(library);
-
-
-// To do
-
-// list out each book from the library's information.
-// display library books on screen
-// build a form for adding books instead of a prompt
-// form 
-//    - title field
-//    - author field
-//    - read/unread radio buttons
-//    - ADD BOOK button
-//    - 'Is this information correct?' modal to confirm ( yes / no ) buttons
-//          - initially just re-run get book with blank fields
-//          - eventually allows you to go back and correct input
-//
 
