70c076fb47fa4c7f0b22a20a3ee147b7d7d5b798
Steven McIntyre <steven.mc2225@gmail.com>
2024-06-04 07:42:36 -0700

    got the books to store, but now the inputs won't clear?  Not worth focusing on for now I think

--- scripts.js
+++ scripts.js
@@ -6,23 +6,18 @@ Initializing constants
 const library = [];
 const bookshelf = document.getElementById("bookshelf");
 
-// Add new book object to library array
-// const addBookButton = document.querySelector("#button_addBook");
-// addBookButton.addEventListener('click', (event) => {
-//   console.log(event);
-//   getBookInfo();
-// });
-
-// Add all the books from the library array to the page.
-const fillLibraryButton = document.querySelector("#button_fillLibrary");
-fillLibraryButton.addEventListener('click', (event) => {
-  console.log("fill library pressed");
-  showLibraryContents();
-});
-
+// New bookk form elements
 const title = document.getElementById('title').value;
 const author = document.getElementById('author').value;
 const read = document.getElementsByName(`read`).value;
+
+
+// Add all the books from the library array to the page.
+const addBook = document.getElementById("button_addBook")
+addBook.addEventListener('click', getBookInfo);
+
+const fillLibraryButton = document.querySelector("#button_fillLibrary");
+fillLibraryButton.addEventListener('click', showLibraryContents);
 
 /*
 Constructors
@@ -45,12 +40,13 @@ function addBookToLibrary(bookObject) { // add book to library
   console.log(library[0].read);
 };
 
-function getBookInfo() { //make new book and add to library uses prompts for user input
+function getBookInfo(event) { //make new book and add to library uses prompts for user input
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const read = document.querySelector('input[name="read"]:checked').value;
   const book = new Book(title, author, read);
   addBookToLibrary(book);
+  event.preventDefault(); // this works, but stops the inputs from clearing after submission...
 }
 
 
@@ -58,6 +54,8 @@ function getBookInfo() { //make new book and add to library uses prompts for use
 function showLibraryContents() {
   console.log("show Library Contents function running");
   console.log(library);
+  console.log(library[0]);
+  console.log(library[1]);
   for (const book in library) {
     if (library.hasOwnProperty(book)) {
       const bookElement = library[book];
