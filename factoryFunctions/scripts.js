cfe1b3324ef8895113bf989c6b0fe30ca39ba8d1
Steven McIntyre <steven.mc2225@gmail.com>
2024-06-09 08:15:06 -0700

    started working on factory functions lesson

--- factoryFunctions/scripts.js
+++ factoryFunctions/scripts.js
@@ -1,2 +1,21 @@
 // Factory Functions
-console.log("test");
+
+let globalAge  = 23; // Globally scoped variable
+
+function printAge (age) {
+  var varAge = 34; // Function scoped variable
+
+  // this is another 'curly brace', thus a block
+  if (age > 0) {
+    // This is a block-scoped variable that exists
+    // within its nearest enclosing block, the if's block
+    const constAge = age * 2;
+    console.log(constAge);
+  }
+
+  // ERROR! We tried to access a block scoped variable
+  // not within its scope
+  console.log(constAge);
+}
+
+printAge(globalAge);
