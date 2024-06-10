65df4cf99a67a610792cc2458ea81a108f723d5b
Steven McIntyre <steven.mc2225@gmail.com>
2024-06-10 07:23:56 -0700

    trying to make dynamic size changer still, but function is running when it shouldn't?

--- factoryFunctions/scripts.js
+++ factoryFunctions/scripts.js
@@ -1,15 +1,22 @@
 // Factory Functions
 function sizeChanger (size) {
-  function addNum() {
-    document.body.style.fontSize = `${size}px`;
+  return function addNum(plusSize) { // this is running on page load... why???
+    console.log(plusSize);
+    console.log(size + plusSize);
+    return document.body.style.fontSize = `${size + plusSize}px`;
   };
-  return addNum;
 };
 
-const size12 = sizeChanger(12);
-const size14 = sizeChanger(14);
-const size16 = sizeChanger(16);
+const size12init = sizeChanger(10);
+const size12= size12init(0);
+const size14init = sizeChanger(12);
+const size14= size14init(0);
+const size16init = sizeChanger(14);
+const size16= size16init(0);
+const size16plusinit = sizeChanger(16);
+const size16plus= sizeChanger(6);
 
 document.getElementById("size-12").onclick = size12;
 document.getElementById("size-14").onclick = size14;
 document.getElementById("size-16").onclick = size16;
+document.getElementById("size-16-plus").onclick = size16plus;
