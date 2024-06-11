b22d22c0a907a49768457cc4f102a05b20491786
Steven McIntyre <steven.mc2225@gmail.com>
2024-06-11 07:42:28 -0700

    fixed main function so that I don't have to enter a '0' in order for the bind function to work

--- factoryFunctions/scripts.js
+++ factoryFunctions/scripts.js
@@ -2,6 +2,10 @@
 
 function sizeChanger (size) {
   return function addNum(sizeModifier=0) {
+    if(isNaN(sizeModifier)) { // bind fills sizeModifier with click object of no nummber given
+      sizeModifier = 0;       // this rebinds size modifier to zero so that it can still run.
+    };
+    console.log(isNaN(sizeModifier));
     console.log(`Font size modifier is: ${sizeModifier}`);
     console.log(`Font size is now: ${size + sizeModifier}`);
     return document.body.style.fontSize = `${size+sizeModifier}px`;
@@ -13,8 +17,8 @@ const size14= sizeChanger(14);
 const size16 = sizeChanger(16);
 const size16plus = sizeChanger(16);
 
-document.getElementById("size-12").onclick = size12.bind(sizeChanger, 0);
-document.getElementById("size-14").onclick = size14.bind(sizeChanger, 0);
-document.getElementById("size-16").onclick = size16.bind(sizeChanger, 0);
+document.getElementById("size-12").onclick = size12.bind(sizeChanger);
+document.getElementById("size-14").onclick = size14.bind(sizeChanger);
+document.getElementById("size-16").onclick = size16.bind(sizeChanger);
 document.getElementById("size-16-plus").onclick = size16.bind(sizeChanger, 6);
 
