baa139914a04a8e8d075b314e57a6841a211d493
Steven McIntyre <steven.mc2225@gmail.com>
2024-06-12 09:48:10 -0700

    commented out logging from last assignment and started next lesson section

--- factoryFunctions/scripts.js
+++ factoryFunctions/scripts.js
@@ -5,9 +5,9 @@ function sizeChanger (size) {
     if(isNaN(sizeModifier)) { // bind fills sizeModifier with click object of no nummber given
       sizeModifier = 0;       // this rebinds size modifier to zero so that it can still run.
     };
-    console.log(isNaN(sizeModifier));
-    console.log(`Font size modifier is: ${sizeModifier}`);
-    console.log(`Font size is now: ${size + sizeModifier}`);
+    // console.log(isNaN(sizeModifier));
+    // console.log(`Font size modifier is: ${sizeModifier}`);
+    // console.log(`Font size is now: ${size + sizeModifier}`);
     return document.body.style.fontSize = `${size+sizeModifier}px`;
   };
 };
@@ -22,3 +22,18 @@ document.getElementById("size-14").onclick = size14.bind(sizeChanger);
 document.getElementById("size-16").onclick = size16.bind(sizeChanger);
 document.getElementById("size-16-plus").onclick = size16.bind(sizeChanger, 6);
 
+
+//
+// Factory Function Shorthand and other formats
+//
+
+const name = "Bob";
+const age = 28;
+const color = "red";
+
+// The two variable assignments do the same thing as long as the name is the same as the assignment name.
+const thatObject = { name: name, age: age, color: color }; // old
+const nowFancyObject = {name, age, color}; // new
+
+console.log(thatObject);
+console.log(nowFancyObject);
